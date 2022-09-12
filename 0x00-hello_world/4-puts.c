2041007bb56e1f3f609cda16e75e58394d382337
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-12 10:10:30 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/* This script print a text and exits */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n")
+}

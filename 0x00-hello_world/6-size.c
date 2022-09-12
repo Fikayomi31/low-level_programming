e6c7ec99bf251f862c2a6de08ccf2e1ad26fd27b
Fikayomi31 <fikayoogundijo@gmail.com>
2022-09-12 10:39:13 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+/* Script that print and exits */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long));
+	return (0);
+}
+

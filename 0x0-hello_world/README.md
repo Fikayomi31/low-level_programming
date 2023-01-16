ecffdb5f51c261596073cd82c1ed47a6c2988c25
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-16 09:55:05 +0100

    script

--- 0x0-hello_world/README.md
+++ 0x0-hello_world/README.md
@@ -1,4 +1,4 @@
 # 0x0.C Hello, World
 0. Writing a script that run a C file through the preprocessor and save the result into another file
 1. Writing a script that compiles a C file but does not link
-2. 
+2. Script that generate assembly code for C code and save it in an output file 
---
+++ 0x0-hello_world/main.s
@@ -0,0 +1,21 @@
+	.text
+	.file	"main.c"
+	.globl	main                            // -- Begin function main
+	.p2align	2
+	.type	main,@function
+main:                                   // @main
+	.cfi_startproc
+// %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	mov	w0, wzr
+	str	wzr, [sp, #12]
+	add	sp, sp, #16
+	.cfi_def_cfa_offset 0
+	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        // -- End function
+	.ident	"clang version 15.0.6"
+	.section	".note.GNU-stack","",@progbits

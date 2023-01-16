28ef5725566bc598970595ccc4b98f41e3865653
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-16 18:34:56 +0100

    base16

--- 0x1-variables_if_else_while/8-print_base16.c
+++ 0x1-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 /**
- * main - print all single digit number of base 10 with putchar only
+ * main - Number in base 16 in lowercase
  *
- * Return: Alway 0
+ * Return: Always 0
  */
 int main(void)
 {
 	int i;
+	char ch;
 	for (i = 48; i < 58; i++)
 		putchar(i);
+	for (ch = 'a'; ch < 'g'; ch++)
+		putchar(ch);
 	putchar('\n');
 
 	return (0);

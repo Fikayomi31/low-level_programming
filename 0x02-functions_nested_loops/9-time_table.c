01db3f8c05a1d6286d9b74cd11a4b0e34ac6ee3f
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-20 23:35:23 +0100

    mulit

---
+++ 0x02-functions_nested_loops/9-time_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * time_table - function to print9 times table
+ *
+ * Return: Alway 0
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+
+			if (j / 10)
+				_putchar(48 + (j / 10));
+			else if (j != 0)
+				_putchar(' ');
+
+			_putchar(48 + (k % 10));
+
+			if (j != 9)
+			{
+			
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
+		_putchar('\n');
+	}
+}

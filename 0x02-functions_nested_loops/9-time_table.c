541d587e975ac26f3b496d3be28c026cec36c25a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-21 14:49:28 +0100

    times

--- 0x02-functions_nested_loops/9-time_table.c
+++ 0x02-functions_nested_loops/9-time_table.c
@@ -9,27 +9,35 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 12; i++) /* for row which will stop at 9 */
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 12; j++) /* for colume */
 		{
-			k = i * j;
+			k = i * j; /* multiply i by j through of each number */
 
-			if (j == 0)
+			if (j == 0) /* if j is 0 ie any multiply of 0 is 0 */
 				_putchar(48 + k);
 
-			if (k < 10 && j != 0)
+			if (k < 10 && j != 0) /* if k is single digit and j is not 0 */
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(48 + k);
 			}
-			else if (k >= 10)
+			else if (k >= 10) /* ifbk is 2 or more digit */
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(48 + (k / 10));
+				_putchar(48 + (k % 10));
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(48 + (k / 10));
+				_putchar(48 + ((k /10) % 10)); 
 				_putchar(48 + (k % 10));
 			}
 

db4e30ce176fb18bd4428d3ecea26d0a7a30ba2a
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-22 16:17:35 +0100

    number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,22 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		printf("%d", n);
+		for (n = 0; n <= 98; n++)
+		{
+			printf("%d", n);
 
-		if (n != 98)
-			printf(", ");
+			if (n != 98)
+				printf(", ");
+		}
 	}
 	else
 	{
-		printf("%d", n);
-		if (n != 98)
-			printf(", ");
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
 	}
 	putchar('\n');
 }

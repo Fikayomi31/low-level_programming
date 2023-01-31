641086679d81e0ee8176f2b2df07b3b10edcc461
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-31 13:26:44 +0100

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,17 @@ void more_numbers(void)
 {
 	int c;
 	int i;
-
+	
+	/* in 10 places */
 	for (i = 0; i <= 10; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
+			/* for 2 digit number */
 			if (c >= 10)
-				_putchar(c / 10 + 48);
-			_putchar(c % 10 + 48);
+				_putchar(c / 10 + 48); /*ie 14 / 10 = 1 */
+			/* if it now single digit 2 % 10 is 2 */
+			_putchar(c % 10 + 48); /* ie 14 % 10 = 4 now 14 */
 		}
 		_putchar('\n');
 	}

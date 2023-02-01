720a6846a4357ecc710a6ba5656f70784edafdb0
Fikayomi31 <fikayoogundijo@gmail.com>
2023-02-01 04:19:23 +0100

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,11 +10,15 @@
 void print_triangle(int size)
 {
 	int i, j;
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
-		for (j = 0; j < i; j++)
+		 
+		for (j = 0; j <= size; j++)
 		{
-			_putchar('#');
+			if (j + i <= size)
+				_putchar(' ');
+			else
+				_putchar('#');
 		}
 		_putchar('\n');
 	}

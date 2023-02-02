dd38bb0b6a9ed1253c8d9ad809abfd891a58f400
Fikayomi31 <fikayoogundijo@gmail.com>
2023-02-02 18:23:43 +0100

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -14,10 +14,13 @@ int main(void)
 	long c;
 	long num = 612852475143;
 
+	/* Looping through num of which start on 2 */
 	for (c = 2; c < num; c++)
 	{
+		/* Checking for prime number */
 		while (num % c == 0)
 		{
+			/* checking for largest prime number ie when num is longer divible by c*/
 			num = num / c;
 		}
 	}
@@ -26,4 +29,6 @@ int main(void)
 
 	return (0);
 
+
 }
+

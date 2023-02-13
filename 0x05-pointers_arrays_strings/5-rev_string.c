71181dc0acdf89ae95d336a24955f26c0ffd1e10
Fikayomi31 <fikayoogundijo@gmail.com>
2023-02-13 21:42:55 +0100

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,12 +8,15 @@
  */
 void rev_string(char *s)
 {
+	/* getting the length of the string */
 	int len = strlen(s);
 	int i;
 	char temp;
+	
+	/* looping through the len of the string / 2 to rev the string */
 	for (i = 0; i < len / 2; i++)
 	{
-		temp = s[i];
+		temp = s[i]; /* storing the rev string in temp */
 		s[i] = s[len - i - 1];
         	s[len - i - 1] = temp;
 	}

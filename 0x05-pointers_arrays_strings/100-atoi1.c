c9a08780ab1245a13b865fd657f0fa3a30334a18
Fikayomi31 <fikayoogundijo@gmail.com>
2023-02-17 00:10:56 +0100

    commitv

--- 0x05-pointers_arrays_strings/100-atoi1.c
+++ 0x05-pointers_arrays_strings/100-atoi1.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
- * _atoi1 - strings to integer
- * @s: string to convert
- *
- * Return: string converted to integer
+ * _atoi - convert a string to an integer.
+ * @s: the string to be comverted.
+ *Return: The integer value of the comverted string.
  */
 
-int _atoi1(char *s)
+int _atoi(char *s)
 {
-	int i;
-	unsigned int n = 0;
-	for (i = 0; s[i] != '0' && s[i] <= '9'; ++i)
-		n = 10 * n + (s[i] - '0');
-	return (n);
+	int sign = 1;
+	unsigned int num = 0;
 
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
 }

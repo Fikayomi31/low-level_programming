6bf55c8e3d66c5daf070a6c4a9dc9f1135877882
Fikayomi31 <fikayoogundijo@gmail.com>
2023-02-20 08:50:08 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp1.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp1 - compare two string
+ * @s1: first string
+ * @s2: second string
+ * Return: an integer
+ */
+
+int _strcmp1(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return (0);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,8 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncat1(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncpy1(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+int _strcmp1(char *s1, char *s2);
 
 
 #endif /* MAIN_H */

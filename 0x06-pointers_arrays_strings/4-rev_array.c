d69beb7aa7303bf6228a7cb2c699fd9608074830
Fikayomi31 <fikayoogundijo@gmail.com>
2023-02-22 12:16:38 +0100

    last

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,15 +10,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int first = 0;
-	int last = n - 1;
+	int first = 0; /* to get the first character */
+	int last = n - 1; /* to get the last character */
 	int i;
-
+	
 	while ( first < last)
 	{
-		i = a[first];
-		a[first] = a[last];
-		a[last] = i;
+		i = a[first]; /* casting first string into i */
+		a[first] = a[last]; /*casting into a[first] */
+		a[last] = i; /* casting into a[last] */
 		first++;
 		last--;
 	}

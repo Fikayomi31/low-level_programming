fedb98402acb5eb57fbb62d1f4a7121ff370a0eb
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-18 17:24:40 +0100

    add

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -
+ * @argc: the number of agrument command
+ * @argv: the array argument pass
+ *
+ * Return:
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
+	return (0);
+
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+    int sum = 0;
+    int i, j;
+
+    if (argc == 1) 
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    for (i = 1; i < argc; i++) 
+    {
+        for (j = 0; argv[i][j]; j++) 
+	{
+            if (!isdigit(argv[i][j])) 
+	    {
+                printf("Error\n");
+                return 1;
+            }
+        }
+        sum += atoi(argv[i]);
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
+

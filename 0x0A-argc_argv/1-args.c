0f8fbfa1bc00b333105a257fb7cf4736b6c30bee
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-18 08:15:03 +0100

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print number of argument pass
+ * @argc: the integer agrument
+ * @argv: the string argument
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

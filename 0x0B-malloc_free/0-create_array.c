e6fe1ceb99a39ce0c93a9eb78816a53a12a08567
Fikayomi31 <fikayoogundijo@gmail.com>
2023-03-25 20:10:18 +0100

    create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * create_array - that creates an array of chars, and initializes 
+ * it with a specific char
+ * @size:
+ * @c:
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+}

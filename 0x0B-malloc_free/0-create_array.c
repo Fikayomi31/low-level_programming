314c06fc556bdaead9888e14bed59597caa39e81
Fikayomi31 <fikayoogundijo@gmail.com>
2023-04-10 10:49:31 +0100

    create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,5 +10,20 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int i;
+	
+	
+	if (size == 0)
+		return (NULL);
+	
+	/* creat a malloc */
+	array = malloc(sizeof(c) * size);
 
+	if (array == NULL)
+		return (0);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
 }

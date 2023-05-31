0c318c0e561c2ebdc6dc03c6621378569f3d773f
Fikayomi31 <fikayoogundijo@gmail.com>
2023-05-31 18:14:34 +0000

    2-array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,15 +15,20 @@ int **alloc_grid(int width, int height)
 	/* looping for the colummn */
 	for (i = 0; i < height; i++)
 	{
-		/* allocating memory to thr column */
+		/* allocating memory to the column */
 		grid[i] = malloc(width * sizeof(**grid));
-		if (grid[i] == NULL)
+		/* check if allcated memory is 0 */
+		if (grid[i] == 0)
 		{
+			/* reverse looping */
 			for (i--; i >= 0; i--)
+				/* free the memory for which row */
 				free(grid[i]);
+			/* free memory for 2-dimensional array */
 			free(grid);
 			return (NULL);
 		}
+		/* initializing to 0 */
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}

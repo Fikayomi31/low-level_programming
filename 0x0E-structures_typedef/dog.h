d4f9d51bc5671477b0e2d9ff74ef3f40b5c6f5da
Fikayomi31 <fikayoogundijo@gmail.com>
2023-07-12 23:18:59 +0000

    task 1 for header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+
+/**
+ * struct dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
